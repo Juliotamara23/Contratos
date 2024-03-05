ee40d612d5c253470aea9ea652a0301e26ec63f5
JULIO ANDRES TAMARA HERNANDEZ <julio.tamara23@outlook.com>
2024-03-05 11:37:20 -0500

    Configuracion de usuariosController para crear usuarios obteniendo la informacion de los roles disponibles para el sistema

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -5,7 +5,6 @@ export const Usuarios = async (req, res) => {
   res.render("usuarios", { roles: roles });
 };
 
-
 export const listUsuarios = async (req, res) => {
   const [rows] = await pool.query("SELECT usuarios.*, rol_usuario.rol_nombre FROM usuarios JOIN rol_usuario ON usuarios.rol = rol_usuario.id_rol");
   res.render("tabla_usuarios", { usuarios: rows });
@@ -13,16 +12,32 @@ export const listUsuarios = async (req, res) => {
 
 export const createUsuario = async (req, res) => {
   const newUsuario = req.body;
-  await pool.query("INSERT INTO usuarios set ?", [newUsuario]);
+
+  // Asegúrate de que el nombre del rol proporcionado existe en la tabla rol_usuario
+  const [rol] = await pool.query("SELECT id_rol FROM rol_usuario WHERE rol_nombre = ?", [newUsuario.rol]);
+
+  if (rol.length === 0) {
+    // Maneja la situación si el rol no existe
+    console.error('El rol especificado no existe en la tabla rol_usuario');
+    res.status(400).send('El rol especificado no existe en la tabla rol_usuario');
+    return;
+  }
+
+  // Asigna el id_rol correcto al nuevo usuario
+  newUsuario.rol = rol[0].id_rol;
+
+  // Realiza la inserción en la tabla usuarios
+  await pool.query("INSERT INTO usuarios SET ?", [newUsuario]);
+
   res.redirect("/");
 };
 
 export const editUsuario = async (req, res) => {
   const { id } = req.params;
-  const [result] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [
+  const [result] = await pool.query("SELECT usuarios.*, rol_usuario.rol_nombre FROM usuarios JOIN rol_usuario ON usuarios.rol = rol_usuario.id_rol WHERE id = ?", [
     id,
   ]);
-  res.render("usuarios_edit", { usuario: result[0] });
+  res.render("usuarios_edit", { usuarios: result[0] });
 };
 
 export const updateUsuario = async (req, res) => {
