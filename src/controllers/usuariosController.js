61f7a3976c0942af31010bdf81f69bdeef72692c
JULIO ANDRES TAMARA HERNANDEZ <julio.tamara23@outlook.com>
2024-03-05 12:24:55 -0500

    Configuracion de usuarioController para obtener el id del rol correspondiente del usuario para poder efectuar la consulta update

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -21,10 +21,8 @@ export const createUsuario = async (req, res) => {
     res.status(400).send('El rol especificado no existe en la tabla rol_usuario');
     return;
   }
-
   // Asigna el id_rol correcto al nuevo usuario
   newUsuario.rol = rol[0].id_rol;
-
   // Realiza la inserción en la tabla usuarios
   await pool.query("INSERT INTO usuarios SET ?", [newUsuario]);
 
@@ -35,13 +33,11 @@ export const editUsuario = async (req, res) => {
   const { id } = req.params;
 
   try {
-    // Obtener información del usuario y roles
+
     const [result] = await pool.query("SELECT usuarios.*, rol_usuario.rol_nombre FROM usuarios JOIN rol_usuario ON usuarios.rol = rol_usuario.id_rol WHERE id = ?", [id]);
 
-    // Obtener todos los roles disponibles
     const [roles] = await pool.query("SELECT * FROM rol_usuario");
 
-    // Renderizar la vista con la información del usuario y roles
     res.render("usuarios_edit", { usuarios: result[0], roles });
   } catch (error) {
     console.error('Error al ejecutar la consulta:', error);
@@ -49,13 +45,26 @@ export const editUsuario = async (req, res) => {
   }
 };
 
-
 export const updateUsuario = async (req, res) => {
   const { id } = req.params;
   const newUsuario = req.body;
+
+  const [rol] = await pool.query("SELECT id_rol FROM rol_usuario WHERE rol_nombre = ?", [newUsuario.rol]);
+
+  if (rol.length === 0) {
+    // Maneja la situación si el rol no existe
+    console.error('El rol especificado no existe en la tabla rol_usuario');
+    res.status(400).send('El rol especificado no existe en la tabla rol_usuario');
+    return;
+  }
+  // Asigna el id_rol correcto al nuevo usuario
+  newUsuario.rol = rol[0].id_rol;
+  // Realiza la inserción en la tabla usuarios
   await pool.query("UPDATE usuarios set ? WHERE id = ?", [newUsuario, id]);
+
   res.redirect("/");
 };
+
 
 export const deleteUsuario = async (req, res) => {
   const { id } = req.params;
